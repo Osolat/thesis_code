8e2beea4d38d1bffa04e53a834487418c6f14c0e
Benjamin Hansen <bbhbenjamin@gmail.com>
2022-03-09 15:22:09 +0100

    test_access_tree is now a failing compilation test

--- Makefile
+++ Makefile
@@ -6,7 +6,9 @@ CXXFLAGS = -O3 -funroll-loops -fomit-frame-pointer -finline-small-functions -mar
 CXX = g++
 
 RELIC_LIB_BLS_12_381=/usr/local/lib/librelic_s.a
-RELIC_INCLUDE_BLS_12_381=usr/local/include/
+RELIC_INCLUDE_BLS_12_381=/usr/local/include/
+#RELIC_LIB_BLS_12_381=~/Master-Thesis/relic/relic-target/lib/librelic_s.a
+#RELIC_INCLUDE_BLS_12_381=~/Master-Thesis/
 
 ARITH_OBJ = zp_arith.o g1_arith.o g2_arith.o gt_arith.o structures.o pairing_arith.o
 LEGACY_OBJ = l_zobject.o l_zfunctioninput.o l_zattributelist.o l_zpolicy.o l_zdriver.o zscanner.o zparser.tab.o l_zgroup.o l_zelement.o l_zelement_bp.o l_zlsss.o
@@ -32,15 +34,19 @@ main:
 	cc $(CXXFLAGS) -I$(RELIC_INCLUDE_BLS_12_381) -fPIC  -O3 -DSSL_LIB_INIT  -Wno-implicit-function-declaration  -c legacy/arith/l_zelement.c -o l_zelement.o
 	g++ $(CXXFLAGS) -c legacy/arith/l_zelement_bp.cpp -I$(RELIC_INCLUDE_BLS_12_381)
 	g++ $(CXXFLAGS) -c legacy/lsss/l_zlsss.cpp -I$(RELIC_INCLUDE_BLS_12_381)
-	$(CXX) -o objects/main $(CXXFLAGS) $(ARITH_OBJ) $(POLICY_OBJ) $(LEGACY_OBJ) -I$(RELIC_INCLUDE_BLS_12_381) run_kp_gpsw_lu.cpp $(RELIC_LIB_BLS_12_381) -lgmp
+	$(CXX) -o objects/main $(CXXFLAGS) $(ARITH_OBJ) $(POLICY_OBJ) $(LEGACY_OBJ) -I$(RELIC_INCLUDE_BLS_12_381) run_kp_gpsw.cpp $(RELIC_LIB_BLS_12_381) -lgmp
 	rm *.o	
 
 policy_tree:
 	$(info ************  Compiling Policy Tree Test************)
 	g++ $(CXXFLAGS) -c lib/policy/policy_tree.cpp -I$(RELIC_INCLUDE_BLS_12_381)
-	$(CXX) -o objects/main $(CXXFLAGS) $(POLICY_OBJ) -I$(RELIC_INCLUDE_BLS_12_381) test_access_tree.cpp $(RELIC_LIB_BLS_12_381) -lgmp
+	$(CXX) -o objects/main $(CXXFLAGS) -I$(RELIC_INCLUDE_BLS_12_381) test_access_tree.cpp $(RELIC_LIB_BLS_12_381) -lgmp
 	rm *.o
 	
+failing_comp:
+	$(info ************  Compiling Policy Tree Test************)
+	$(CXX) -o objects/main $(CXXFLAGS) -I$(RELIC_INCLUDE_BLS_12_381) test_access_tree.cpp $(RELIC_LIB_BLS_12_381) -lgmp
+	rm *.o
 clean:
 	rm -f objects/* main *~
 
--- test_access_tree.cpp
+++ test_access_tree.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
-#include "lib/policy/policy_tree.h"
-
-int test1() {
+//#include "lib/policy/policy_tree.h"
+extern "C" {
+#include <relic/relic.h>
+}
+/* int test1() {
     char formula[] = "AND(OR(attr1),OR(attr2),OR(OR(attr3),OR(attr4)))";
     struct node root = node();
     tree_from_string(formula, &root);
@@ -47,22 +49,68 @@ int test5() {
     std::cout << formula << std::endl;
     print_tree(&root);
     return EXIT_SUCCESS;
-}
+} */
+
+/* int test6() {
+    bn_t secret, recovered, x[10], y[10];
+    bn_null(secret);
+    bn_new(secret);
+    bn_null(recovered);
+    bn_new(recovered);
+    bn_t order;
+    bn_null(order);
+    bn_new(order);
+    bn_set_dig(secret, 99);
+    for (size_t i = 0; i < 10; i++) {
+        bn_null(y[i]);
+        bn_new(y[i]);
+        bn_null(x[i]);
+        bn_new(x[i]);
+    }
+    mpc_sss_gen(x, y, secret, order, 1, 10);
+    mpc_sss_key(recovered, x, y, order, 1);
+    bn_print(recovered);
+} */
 
 int main(int argc, char const *argv[]) {
-    std::cout << "Test 1 " << std::endl;
-    test1();
-    std::cout << "--------------------- " << std::endl;
-    std::cout << "Test 2 " << std::endl;
-    test2();
-    std::cout << "--------------------- " << std::endl;
-    std::cout << "Test 3 " << std::endl;
-    test3();
-    std::cout << "--------------------- " << std::endl;
-    std::cout << "Test 4 " << std::endl;
-    test4();
-    std::cout << "--------------------- " << std::endl;
-    std::cout << "Test 5 " << std::endl;
-    test5();
+    /*   std::cout << "Test 1 " << std::endl;
+      test1();
+      std::cout << "--------------------- " << std::endl;
+      std::cout << "Test 2 " << std::endl;
+      test2();
+      std::cout << "--------------------- " << std::endl;
+      std::cout << "Test 3 " << std::endl;
+      test3();
+      std::cout << "--------------------- " << std::endl;
+      std::cout << "Test 4 " << std::endl;
+      test4();
+      std::cout << "--------------------- " << std::endl;
+      std::cout << "Test 5 " << std::endl;
+      test5(); */
+    bn_t secret, recovered, x[10], y[10];
+    bn_null(secret);
+    bn_new(secret);
+    bn_null(recovered);
+    bn_new(recovered);
+    bn_t order;
+    bn_null(order);
+    bn_new(order);
+    bn_set_dig(secret, 99);
+    for (size_t i = 0; i < 10; i++) {
+        bn_null(y[i]);
+        bn_new(y[i]);
+        bn_null(x[i]);
+        bn_new(x[i]);
+    }
+    mpc_sss_gen(x, y, secret, order, 1, 10);
+    mpc_sss_key(recovered, x, y, order, 1);
+    //These are just to see if any function from relic_mpc compiles
+    mpc_mt_gen(NULL, NULL);
+    mpc_mt_lcl(NULL, NULL, NULL, NULL, NULL, NULL);
+    mpc_mt_bct(NULL, NULL, NULL);
+    mpc_mt_mul(NULL, NULL, NULL, NULL, NULL, NULL);
+    md_map_sh512(NULL, NULL, NULL);
+    rand_bytes(NULL,NULL);
+    bn_print(recovered);
     return 0;
 }
