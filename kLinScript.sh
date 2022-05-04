35c8b8509bb5e3d542c084326e69785bd35bc636
jonas <jonashaugaard.s@gmail.com>
2022-05-04 10:27:47 +0200

    Fixed Key Gen across all variants, and fixed decryption for unbounded variants. Also added (two sPP + Inv) benchmark.

---
+++ kLinScript.sh
@@ -0,0 +1,22 @@
+cd objects
+ulimit -s unlimited &&
+./klin_kp_std 2 >> klinNew.txt && ./klin_kp_std 4 >> klinNew.txt && ./klin_kp_std 8 >> klinNew.txt && ./klin_kp_std 16 >> klinNew.txt && ./klin_kp_std 32 >> klinNew.txt && 
+./klin_kp_std 64 >> klinNew.txt && ./klin_kp_std 128 >> klinNew.txt && ./klin_kp_std 256 >> klinNew.txt && ./klin_kp_std 512 >> klinNew.txt && ./klin_kp_std 1024 >> klinNew.txt && ./klin_kp_G 2 >> klinNew.txt && 
+./klin_kp_G 4 >> klinNew.txt && ./klin_kp_G 8 >> klinNew.txt && ./klin_kp_G 16 >> klinNew.txt && ./klin_kp_G 32 >> klinNew.txt && ./klin_kp_G 64 >> klinNew.txt && ./klin_kp_G 128 >> klinNew.txt && 
+./klin_kp_G 256 >> klinNew.txt && ./klin_kp_G 512 >> klinNew.txt && ./klin_kp_G 1024 >> klinNew.txt && ./klin_kp_A 2 >> klinNew.txt && ./klin_kp_A 4 >> klinNew.txt && ./klin_kp_A 8 >> klinNew.txt && 
+./klin_kp_A 16 >> klinNew.txt && 
+./klin_kp_A 32 >> klinNew.txt && ./klin_kp_A 64 >> klinNew.txt && ./klin_kp_A 128 >> klinNew.txt && ./klin_kp_A 256 >> klinNew.txt && ./klin_kp_A 512 >> klinNew.txt && ./klin_kp_A 1024 >> klinNew.txt && 
+./klin_kp_P 2 >> klinNew.txt && 
+./klin_kp_P 4 >> klinNew.txt && ./klin_kp_P 8 >> klinNew.txt && ./klin_kp_P 16 >> klinNew.txt && ./klin_kp_P 32 >> klinNew.txt && ./klin_kp_P 64 >> klinNew.txt && ./klin_kp_P 128 >> klinNew.txt && 
+./klin_kp_P 256 >> klinNew.txt && ./klin_kp_P 512 >> klinNew.txt && ./klin_kp_P 1024 >> klinNew.txt && ./klin_kp_GAP 2 >> klinNew.txt && ./klin_kp_GAP 4 >> klinNew.txt && ./klin_kp_GAP 8 >> klinNew.txt && 
+./klin_kp_GAP 16 >> klinNew.txt && 
+./klin_kp_GAP 32 >> klinNew.txt && ./klin_kp_GAP 64 >> klinNew.txt && ./klin_kp_GAP 128 >> klinNew.txt && ./klin_kp_GAP 256 >> klinNew.txt && ./klin_kp_GAP 512 >> klinNew.txt && ./klin_kp_GAP 1024 >> klinNew.txt && 
+./klin_kp_GAP_OE 2 >> klinNew.txt && 
+./klin_kp_GAP_OE 4 >> klinNew.txt && ./klin_kp_GAP_OE 8 >> klinNew.txt && ./klin_kp_GAP_OE 16 >> klinNew.txt && ./klin_kp_GAP_OE 32 >> klinNew.txt &&  ./klin_kp_GAP_OE 64 >> klinNew.txt && 
+./klin_kp_GAP_OE 128 >> klinNew.txt && ./klin_kp_GAP_OE 256 >> klinNew.txt && ./klin_kp_GAP_OE 512 >> klinNew.txt && ./klin_kp_GAP_OE 1024 >> klinNew.txt && ./klin_kp_GAP_OK 2 >> klinNew.txt && 
+./klin_kp_GAP_OK 4 >> klinNew.txt && 
+./klin_kp_GAP_OK 8 >> klinNew.txt && ./klin_kp_GAP_OK 16 >> klinNew.txt && ./klin_kp_GAP_OK 32 >> klinNew.txt && ./klin_kp_GAP_OK 64 >> klinNew.txt && 
+./klin_kp_GAP_OK 128 >> klinNew.txt && ./klin_kp_GAP_OK 256 >> klinNew.txt && ./klin_kp_GAP_OK 512 >> klinNew.txt && ./klin_kp_GAP_OK 1024 >> klinNew.txt && ./klin_kp_GAP_OD 2 >> klinNew.txt && 
+./klin_kp_GAP_OD 4 >> klinNew.txt && 
+./klin_kp_GAP_OD 8 >> klinNew.txt && ./klin_kp_GAP_OD 16 >> klinNew.txt && ./klin_kp_GAP_OD 32 >> klinNew.txt && ./klin_kp_GAP_OD 64 >> klinNew.txt && ./klin_kp_GAP_OD 128 >> klinNew.txt && 
+./klin_kp_GAP_OD 256 >> klinNew.txt && ./klin_kp_GAP_OD 512 >> klinNew.txt && ./klin_kp_GAP_OD 1024 >> klinNew.txt
