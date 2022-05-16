fdc5ec4f8082094d3480d3543acfa4347094f626
Benjamin Hansen <bbhbenjamin@gmail.com>
2022-05-16 21:39:26 +0200

    defs

--- benchmark_alp_a_8_to_64.sh
+++ benchmark_alp_a_8_to_64.sh
@@ -1,5 +1,6 @@
 cd objects
 ulimit -s unlimited &&
+./bench_simultaneous_stuff >> sim_bench.txt &&
 #./alp_naive n_attr 2 >> alp_result.txt &&
 #./alp_g_oe n_attr 2 >> alp_result.txt &&
 #./alp_a_oe n_attr 2 >> alp_result.txt &&
@@ -36,11 +37,11 @@ ulimit -s unlimited &&
 ./alp_p_oe n_attr 64 >> alp_result.txt &&
 ./alp_GAP_oe n_attr 64 >> alp_result.txt &&
 #./alp_GAP_ok n_attr 64 >> alp_result.txt 
-./alp_p_oe n_attr 128 >> alp_result.txt &&
-./alp_GAP_oe n_attr 128 >> alp_result.txt &&
-./alp_p_oe n_attr 256 >> alp_result.txt &&
-./alp_GAP_oe n_attr 256 >> alp_result.txt &&
-./alp_p_oe n_attr 512 >> alp_result.txt &&
-./alp_GAP_oe n_attr 512 >> alp_result.txt &&
-./alp_p_oe n_attr 1024 >> alp_result.txt &&
-./alp_GAP_oe n_attr 1024 >> alp_result.txt &&
+./alp_p_oe_500 n_attr 128 >> alp_result.txt &&
+./alp_GAP_oe_500 n_attr 128 >> alp_result.txt &&
+./alp_p_oe_100 n_attr 256 >> alp_result.txt &&
+./alp_GAP_oe_100 n_attr 256 >> alp_result.txt &&
+./alp_p_oe_10 n_attr 512 >> alp_result.txt &&
+./alp_GAP_oe_10 n_attr 512 >> alp_result.txt &&
+./alp_p_oe_2 n_attr 1024 >> alp_result.txt &&
+./alp_GAP_oe_2 n_attr 1024 >> alp_result.txt 
