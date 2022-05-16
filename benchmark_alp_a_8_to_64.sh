d639a429fca91de6c7f60edbb0325034dbb8b1cc
Turtlemore <turtlemore@gmail.com>
2022-05-16 16:49:13 +0200

    small exponent in group operation pre computation alp

--- benchmark_alp_a_8_to_64.sh
+++ benchmark_alp_a_8_to_64.sh
@@ -1,6 +1,38 @@
 cd objects
 ulimit -s unlimited &&
+./alp_naive n_attr 2 >> alp_result.txt &&
+./alp_g_oe n_attr 2 >> alp_result.txt &&
+./alp_a_oe n_attr 2 >> alp_result.txt &&
+./alp_p_oe n_attr 2 >> alp_result.txt &&
+./alp_GAP_oe n_attr 2 >> alp_result.txt &&
+./alp_GAP_ok n_attr 2 >> alp_result.txt &&
+./alp_naive n_attr 4 >> alp_result.txt &&
+./alp_g_oe n_attr 4 >> alp_result.txt &&
+./alp_a_oe n_attr 4 >> alp_result.txt &&
+./alp_p_oe n_attr 4 >> alp_result.txt &&
+./alp_GAP_oe n_attr 4 >> alp_result.txt &&
+./alp_GAP_ok n_attr 4 >> alp_result.txt &&
+./alp_naive n_attr 8 >> alp_result.txt &&
+./alp_g_oe n_attr 8 >> alp_result.txt &&
+./alp_a_oe n_attr 8 >> alp_result.txt &&
 ./alp_p_oe n_attr 8 >> alp_result.txt &&
+./alp_GAP_oe n_attr 8 >> alp_result.txt &&
+./alp_GAP_ok n_attr 8 >> alp_result.txt &&
+./alp_naive n_attr 16 >> alp_result.txt &&
+./alp_g_oe n_attr 16 >> alp_result.txt &&
+./alp_a_oe n_attr 16 >> alp_result.txt &&
 ./alp_p_oe n_attr 16 >> alp_result.txt &&
+./alp_GAP_oe n_attr 16 >> alp_result.txt &&
+./alp_GAP_ok n_attr 16 >> alp_result.txt &&
+./alp_naive n_attr 32 >> alp_result.txt &&
+./alp_g_oe n_attr 32 >> alp_result.txt &&
+./alp_a_oe n_attr 32 >> alp_result.txt &&
 ./alp_p_oe n_attr 32 >> alp_result.txt &&
+./alp_GAP_oe n_attr 32 >> alp_result.txt &&
+./alp_GAP_ok n_attr 32 >> alp_result.txt &&
+./alp_naive n_attr 64 >> alp_result.txt &&
+./alp_g_oe n_attr 64 >> alp_result.txt &&
+./alp_a_oe n_attr 64 >> alp_result.txt &&
 ./alp_p_oe n_attr 64 >> alp_result.txt &&
+./alp_GAP_oe n_attr 64 >> alp_result.txt &&
+./alp_GAP_ok n_attr 64 >> alp_result.txt 
