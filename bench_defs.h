31e37b720cfc6e2c32efc0a5aae8f9484e579778
Benjamin Hansen <bbhbenjamin@gmail.com>
2022-05-19 16:29:13 +0200

    ntest

--- bench_defs.h
+++ bench_defs.h
@@ -33,7 +33,7 @@ extern "C" {
 
 /* Number of tests per operation during measurement */
 
-#define NTESTS 1000
+#define NTESTS 2000
 #define g1_pre_sim_switchpoint 32
 #define g2_pre_sim_switchpoint 32
 #define exponent_bits_exceed_breakpoint 47
