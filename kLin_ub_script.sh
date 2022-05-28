f56d70c42164d9177b0debc442464fae0452047f
Benjamin Hansen <bbhbenjamin@gmail.com>
2022-05-28 16:18:41 +0200

    beh

--- kLin_ub_script.sh
+++ kLin_ub_script.sh
@@ -8,13 +8,16 @@ ulimit -s unlimited &&
 ./klin_kp_gap_ub_ok_1 32 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_1 64 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_1 128 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_1 256 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_1 512 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_1 1024 >> klin_ub_res.txt && 
 ./klin_kp_gap_ub_od_1 2 >> klin_ub_res.txt && 
 ./klin_kp_gap_ub_od_1 4 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_1 8 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_1 16 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_1 32 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_1 64 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_1 128 >> klin_ub_res.txt && 
-./klin_kp_gap_ub_od_1 256 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_1 512 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_1 1024 >> klin_ub_res.txt && 
+./klin_kp_gap_ub_od_1 256 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_1 512 >> klin_ub_res.txt && 
+./klin_kp_gap_ub_od_1 1024 >> klin_ub_res.txt && 
 ./klin_kp_ub_2 2 >> klin_ub_res.txt && ./klin_kp_ub_2 4 >> klin_ub_res.txt && ./klin_kp_ub_2 8 >> klin_ub_res.txt && ./klin_kp_ub_2 16 >> klin_ub_res.txt && ./klin_kp_ub_2 32 >> klin_ub_res.txt && 
 ./klin_kp_ub_2 64 >> klin_ub_res.txt && ./klin_kp_ub_2 128 >> klin_ub_res.txt && ./klin_kp_ub_2 256 >> klin_ub_res.txt && ./klin_kp_ub_2 512 >> klin_ub_res.txt && ./klin_kp_ub_2 1024 >> klin_ub_res.txt && ./klin_kp_gap_ub_2 2 >> klin_ub_res.txt && 
 ./klin_kp_gap_ub_2 4 >> klin_ub_res.txt && ./klin_kp_gap_ub_2 8 >> klin_ub_res.txt && ./klin_kp_gap_ub_2 16 >> klin_ub_res.txt && ./klin_kp_gap_ub_2 32 >> klin_ub_res.txt && ./klin_kp_gap_ub_2 64 >> klin_ub_res.txt && ./klin_kp_gap_ub_2 128 >> klin_ub_res.txt && 
-./klin_kp_gap_ub_2 256 >> klin_ub_res.txt && ./klin_kp_gap_ub_2 512 >> klin_ub_res.txt && ./klin_kp_gap_ub_2 1024 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 2 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 4 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 8 >> klin_ub_res.txt && 
+./klin_kp_gap_ub_2 256 >> klin_ub_res.txt && 
+./klin_kp_gap_ub_2 512 >> klin_ub_res.txt && ./klin_kp_gap_ub_2 1024 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 2 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 4 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 8 >> klin_ub_res.txt && 
 ./klin_kp_gap_ub_ok_2 16 >> klin_ub_res.txt && 
-./klin_kp_gap_ub_ok_2 32 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 64 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 128 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 256 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 512 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 1024 >> klin_ub_res.txt && 
+./klin_kp_gap_ub_ok_2 32 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 64 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 128 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 256 >> klin_ub_res.txt && 
+./klin_kp_gap_ub_ok_2 512 >> klin_ub_res.txt && ./klin_kp_gap_ub_ok_2 1024 >> klin_ub_res.txt && 
 ./klin_kp_gap_ub_od_2 2 >> klin_ub_res.txt && 
 ./klin_kp_gap_ub_od_2 4 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_2 8 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_2 16 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_2 32 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_2 64 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_2 128 >> klin_ub_res.txt && 
 ./klin_kp_gap_ub_od_2 256 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_2 512 >> klin_ub_res.txt && ./klin_kp_gap_ub_od_2 1024 >> klin_ub_res.txt
